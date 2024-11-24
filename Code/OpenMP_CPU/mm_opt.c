6d6c38b10c1c2e729d9863c6969f7989e9ffe0cb
lorenzorizzi17 <lorenzorizzi17@gmail,com>
2024-11-24 01:44:34 +0100

    Optimization up to 10x

--- Code/OpenMP_CPU/mm_opt.c
+++ Code/OpenMP_CPU/mm_opt.c
@@ -39,23 +39,31 @@
       }
   }
 } */
+
 int MIN(int a, int b){
    if(a<b){ return a;}
    else {return b;}
 }
+
 #define MAX_PAGE 4096
 
 
 
 void mm_opt(int Ndim, int Mdim, int Pdim, TYPE *A, TYPE *B, TYPE *C){ 
-   TYPE tmp;
-
-   //calculate tile_size
+   
+   ////////////////////////////////////////////////////////////////
+   ////     First things first, transpose the matrix B.
+   ////     This way, when performing the matrix multiplication
+   ////     every row of A gets multiplied by a row of B,
+   ////     assuring cache locality (rows are continguous in memory)
+   /////////////////////////////////////////////////////////////////
+   
+   //Calculate tile_size (page size)
    int tile_size_i = sqrt(Mdim*MAX_PAGE / (double)(Pdim*sizeof(TYPE)));
    int tile_size_j = sqrt(Pdim*MAX_PAGE / (double)(Mdim*sizeof(TYPE)));
-   //transpose B
+   
+   //Effectively transpose B using tiles
    TYPE* Bt = (TYPE*) malloc(Pdim*Mdim*sizeof(TYPE));
-   int tile_size = 400/10;
    for (int i=0; i<Pdim; i+=tile_size_i) {
       for (int j=0; j<Mdim; j+=tile_size_j) {
          for (int it=i; it< MIN(Pdim,i+tile_size_i); it++){
@@ -66,25 +74,56 @@ void mm_opt(int Ndim, int Mdim, int Pdim, TYPE *A, TYPE *B, TYPE *C){
       }
    }
 
+   omp_set_num_threads(8);
+   int nthreads;
 
-   for (int i=0; i < Ndim; i++){
-      for (int j=0; j < Mdim; j++){
-            tmp = 0.0;
-            // formally, C_ij = A_ik B_kj, where k goes from 1 to P
-            //vectorization using 512 bit registers?
-            for(int k=0;k<Pdim; k=k+8){
-               TYPE tmp1 = *(A+(i*Pdim+k)) *  *(Bt+(j*Pdim+k));   
-               TYPE tmp2 = *(A+(i*Pdim+k+1)) *  *(Bt+(j*Pdim+k+1));
-               TYPE tmp3 = *(A+(i*Pdim+k+2)) *  *(Bt+(j*Pdim+k+2));   
-               TYPE tmp4 = *(A+(i*Pdim+k+3)) *  *(Bt+(j*Pdim+k+3));
-               TYPE tmp5 = *(A+(i*Pdim+k+4)) *  *(Bt+(j*Pdim+k+4));  
-               TYPE tmp6 = *(A+(i*Pdim+k+5)) *  *(Bt+(j*Pdim+k+5));
-               TYPE tmp7 = *(A+(i*Pdim+k+6)) *  *(Bt+(j*Pdim+k+6));  
-               TYPE tmp8 = *(A+(i*Pdim+k+7)) *  *(Bt+(j*Pdim+k+7));
-               tmp += tmp1+tmp2+tmp3+tmp4+tmp5+tmp6+tmp7+tmp8;
-            }
-            *(C+(i*Mdim+j)) += tmp;
+   #pragma omp parallel
+   {
+      int id = omp_get_thread_num();
+
+      if (id == 0){ nthreads = omp_get_num_threads();}
+
+      //i cycle
+      for (int i=id; i < Ndim; i+= nthreads){         
+         //j cycle
+         for (int j=i; j < Mdim; j++){
+            TYPE tmp = 0; 
+               // Vectorization using 512-bit registers?
+               // Something like AVX512, I'm not sure whether my computer have those 
+               // but I've noticed quite a performance boost when going from 4 sums to 8 sums
+               for(int k=0;k<Pdim; k=k+8){
+                  TYPE tmp1 = *(A+(i*Pdim+k+0)) *  *(Bt+(j*Pdim+k+0));  
+                  TYPE tmp2 = *(A+(i*Pdim+k+1)) *  *(Bt+(j*Pdim+k+1));
+                  TYPE tmp3 = *(A+(i*Pdim+k+2)) *  *(Bt+(j*Pdim+k+2));   
+                  TYPE tmp4 = *(A+(i*Pdim+k+3)) *  *(Bt+(j*Pdim+k+3));
+                  TYPE tmp5 = *(A+(i*Pdim+k+4)) *  *(Bt+(j*Pdim+k+4));  
+                  TYPE tmp6 = *(A+(i*Pdim+k+5)) *  *(Bt+(j*Pdim+k+5));
+                  TYPE tmp7 = *(A+(i*Pdim+k+6)) *  *(Bt+(j*Pdim+k+6));  
+                  TYPE tmp8 = *(A+(i*Pdim+k+7)) *  *(Bt+(j*Pdim+k+7)); 
+                  tmp += tmp1+tmp2+tmp3+tmp4+tmp5+tmp6+tmp7+tmp8;
+               }
+               *(C+(i*Mdim+j)) += tmp;
+         }
+         for (int j=0; j < i; j++){
+               TYPE tmp = 0.0;
+               // Vectorization using 512-bit registers?
+               // Something like AVX512, I'm not sure whether my computer have those 
+               // but I've noticed quite a performance boost when going from 4 sums to 8 sums
+               for(int k=0;k<Pdim; k=k+8){
+                  TYPE tmp1 = *(A+(i*Pdim+k+0)) *  *(Bt+(j*Pdim+k+0));  
+                  TYPE tmp2 = *(A+(i*Pdim+k+1)) *  *(Bt+(j*Pdim+k+1));
+                  TYPE tmp3 = *(A+(i*Pdim+k+2)) *  *(Bt+(j*Pdim+k+2));   
+                  TYPE tmp4 = *(A+(i*Pdim+k+3)) *  *(Bt+(j*Pdim+k+3));
+                  TYPE tmp5 = *(A+(i*Pdim+k+4)) *  *(Bt+(j*Pdim+k+4));  
+                  TYPE tmp6 = *(A+(i*Pdim+k+5)) *  *(Bt+(j*Pdim+k+5));
+                  TYPE tmp7 = *(A+(i*Pdim+k+6)) *  *(Bt+(j*Pdim+k+6));  
+                  TYPE tmp8 = *(A+(i*Pdim+k+7)) *  *(Bt+(j*Pdim+k+7)); 
+                  tmp += tmp1+tmp2+tmp3+tmp4+tmp5+tmp6+tmp7+tmp8;
+               }
+               *(C+(i*Mdim+j)) += tmp;
+         }
       }
    }
+
 
 }
